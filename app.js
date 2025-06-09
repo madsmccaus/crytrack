d830ddd614feae29d8b0ff74b0499fbbbb2626f4
madsmccaus <mads.mccausland@gmail.com>
2025-06-08 19:55:44 -0700

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ class CryptoDash {
         this.apiBase = 'https://api.coingecko.com/api/v3';
         this.cmcApiBase = 'https://pro-api.coinmarketcap.com/v1';
         // Replace with your own CoinMarketCap API key if available
-        this.cmcApiKey = '';
+        this.cmcApiKey = '99279fc6-7f68-45df-89dd-a7e2c64ddf75';
         this.refreshInterval = 30000; // 30 seconds
         this.currentPage = 'overview';
         this.cryptoData = {};
